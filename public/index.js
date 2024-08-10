2fce2411b66a2cf710c107ca5f1e9639cef4d2f9
Calvin Oduho <calvin.oduho@gmail.com>
2024-08-10 16:31:25 +0300

    Edit index.html filepaths

--- public/index.js
+++ public/index.js
@@ -20,31 +20,31 @@ document.addEventListener("keydown", function(evt) {
 function makeSoundBasedOnThisKey(key) {
     switch (key) {
         case "l":
-            var crash = new Audio("../assets/sounds/kick-bass.mp3");
+            var crash = new Audio("./assets/sounds/kick-bass.mp3");
             crash.play();         
             break;
         case "k":
-            var kickbass = new Audio("../assets/sounds/crash.mp3");
+            var kickbass = new Audio("./assets/sounds/crash.mp3");
             kickbass.play(); 
             break;
         case "j":
-            var snare = new Audio("../assets/sounds/snare.mp3");
+            var snare = new Audio("./assets/sounds/snare.mp3");
             snare.play(); 
             break;
         case "w":
-            var tom1 = new Audio("../assets/sounds/tom-1.mp3");
+            var tom1 = new Audio("./assets/sounds/tom-1.mp3");
             tom1.play(); 
             break;
         case "a":
-            var tom2 = new Audio("../assets/sounds/tom-2.mp3");
+            var tom2 = new Audio("./assets/sounds/tom-2.mp3");
             tom2.play(); 
             break;
         case "s":
-            var tom3 = new Audio("../assets/sounds/tom-3.mp3");
+            var tom3 = new Audio("./assets/sounds/tom-3.mp3");
             tom3.play(); 
             break;
         case "d":
-            var tom4 = new Audio("../assets/sounds/tom-4.mp3");
+            var tom4 = new Audio("./assets/sounds/tom-4.mp3");
             tom4.play(); 
             break;
         default:
